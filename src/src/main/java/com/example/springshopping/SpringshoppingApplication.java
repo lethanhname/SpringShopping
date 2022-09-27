415e0cb60cc1b1b24cb82eec40781562fa3e9d89
Nam Le <lethanhnam010490@gmail.com>
2022-09-27 09:39:57 +0700

    Init project

---
+++ src/src/main/java/com/example/springshopping/SpringshoppingApplication.java
@@ -0,0 +1,13 @@
+package com.example.springshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringshoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringshoppingApplication.class, args);
+	}
+
+}
---
+++ src/src/test/java/com/example/springshopping/SpringshoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringshoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
