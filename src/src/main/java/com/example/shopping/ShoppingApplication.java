c538bd0cba9f80228f4322182f210efd7fe456e9
Nam Le <lethanhnam010490@gmail.com>
2022-09-27 11:34:52 +0700

    rename package

--- src/src/main/java/com/example/shopping/ShoppingApplication.java
+++ src/src/main/java/com/example/shopping/ShoppingApplication.java
@@ -1,13 +1,13 @@
-package com.example.springshopping;
+package com.example.shopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringshoppingApplication {
+public class ShoppingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringshoppingApplication.class, args);
+		SpringApplication.run(ShoppingApplication.class, args);
 	}
 
 }
--- src/src/test/java/com/example/shopping/ShoppingApplicationTests.java
+++ src/src/test/java/com/example/shopping/ShoppingApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.springshopping;
+package com.example.shopping;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringshoppingApplicationTests {
+class ShoppingApplicationTests {
 
 	@Test
 	void contextLoads() {
